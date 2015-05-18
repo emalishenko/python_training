f9ff913fac0bf894cb6cded6d7facb36cd2cf394
Lena Malishenko <e.malishenko@gmail.com>
2015-05-18 12:03:20 +0300

    Extracting of supplementary methods

---
+++ group.py
@@ -0,0 +1,10 @@
+__author__ = 'emalishenko'
+
+
+class Group:
+
+    def __init__(self, name, header, footer):
+        self.name = name
+        self.header = header
+        self.footer = footer
+

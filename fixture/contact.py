168a7c6831e13641d3ab9b60a0a3337db989a6d0
Lena Malishenko <e.malishenko@gmail.com>
2015-05-27 22:02:59 +0300

    Edit contact test added

--- fixture/contact.py
+++ fixture/contact.py
@@ -10,19 +10,7 @@ class ContactHelper:
         wd = self.app.wd
         # init contact creation
         wd.find_element_by_link_text("add new").click()
-        # fill contact form
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.first_name)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.middle_name)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.last_name)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nick)
+        self.fill_contact_form(contact)
         # submit new contact
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
@@ -36,3 +24,29 @@ class ContactHelper:
         wd.find_element_by_xpath("//div[@id='content']/form[2]/input[2]").click()
         # go to homepage
         wd.find_element_by_link_text("home").click()
+
+    def edit_first_contact(self, contact):
+        wd = self.app.wd
+        # select first contact
+        wd.find_element_by_name("selected[]").click()
+        # open contact frm
+        wd.find_element_by_css_selector('img[alt="Edit"]').click()
+        self.fill_contact_form(contact)
+        # submit contact updating
+        wd.find_element_by_xpath("//div[@id='content']/form[1]/input[22]").click()
+
+
+    def fill_contact_form(self, contact):
+        wd = self.app.wd
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.first_name)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middle_name)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.last_name)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(contact.nick)

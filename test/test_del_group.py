7306ab21306743c25583e33fe4693b76e84c1ddf
Lena Malishenko <e.malishenko@gmail.com>
2015-05-27 20:46:26 +0300

    New test for group deleting, common fixture

---
+++ test/test_del_group.py
@@ -0,0 +1,6 @@
+__author__ = 'emalishenko'
+
+def test_delete_first_group(app):
+    app.session.login(user_name="admin", password="secret")
+    app.group.delete_first_group()
+    app.session.logout()

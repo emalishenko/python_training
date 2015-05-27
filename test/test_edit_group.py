6ebdf6930018a4963fb0b8aacbca5c8a375a9007
Lena Malishenko <e.malishenko@gmail.com>
2015-05-27 21:15:58 +0300

    Update group test added

---
+++ test/test_edit_group.py
@@ -0,0 +1,9 @@
+__author__ = 'emalishenko'
+
+from model.group import Group
+
+def test_edit_first_group(app):
+    app.session.login(user_name="admin", password="secret")
+    app.group.edit_first_group(Group(name="Test Grop Update", header="Header text update", footer="Footer text update"))
+    app.session.logout()
+

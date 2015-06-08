aa6887b61c3ca936148bd0fa541ebe18a203a383
Lena Malishenko <e.malishenko@gmail.com>
2015-06-08 19:10:18 +0300

    Added assertion into tests for group. The length of the list is verified

--- test/test_group_modify.py
+++ test/test_group_modify.py
@@ -6,13 +6,22 @@ def test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="To be modified"))
     app.group.modify_first_group(Group(name="New group name"))
+    old_groups = app.group.get_group_list()
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 def test_modify_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(header="To be modified"))
     app.group.modify_first_group(Group(header="New group header"))
+    old_groups = app.group.get_group_list()
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 def test_modify_group_footer(app):
     if app.group.count() == 0:
         app.group.create(Group(footer="To be modified"))
     app.group.modify_first_group(Group(footer="New group footer"))
+    old_groups = app.group.get_group_list()
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)

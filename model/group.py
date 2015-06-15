c8fc39748adb3371043ddebc206aa0cb92880528
Lena Malishenko <e.malishenko@gmail.com>
2015-06-15 19:58:30 +0300

    Test data parametrization for groups

--- model/group.py
+++ model/group.py
@@ -11,7 +11,7 @@ class Group:
         self.id = id
 
     def __repr__(self):
-        return "(%s, %s)" % (self.name, self.id)
+        return "(%s; %s; %s; %s)" % (self.name, self.id, self.header, self.footer)
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and self.name == other.name
